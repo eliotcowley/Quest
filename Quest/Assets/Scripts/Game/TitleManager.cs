eaa594b9f881fd3612f17742a354144dda0da6a5
Eliot Cowley <eliot93@live.com>
2017-02-21 22:15:56 -0800

    Worked on title menu

--- Quest/Assets/Scripts/Game/TitleManager.cs
+++ Quest/Assets/Scripts/Game/TitleManager.cs
@@ -8,8 +8,20 @@ public class TitleManager : MonoBehaviour
     [SerializeField]
     private Button startButton;
 
+    [SerializeField]
+    private GameObject titleMenu;
+
+    [SerializeField]
+    private GameObject optionsMenu;
+
     private void Start()
     {
         startButton.Select();
+    }
+
+    public void ToggleOptions()
+    {
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+        titleMenu.SetActive(!titleMenu.activeSelf);
     }
 }
