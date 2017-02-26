92512689c9b839ea5593029f15428caa97cb5c85
Eliot Cowley <eliot93@live.com>
2017-02-25 22:44:29 -0800

    Added resolution and fullscreen options

--- Quest/Assets/Scripts/Game/TitleManager.cs
+++ Quest/Assets/Scripts/Game/TitleManager.cs
@@ -14,14 +14,67 @@ public class TitleManager : MonoBehaviour
     [SerializeField]
     private GameObject optionsMenu;
 
+    [SerializeField]
+    private Text fullscreenButtonText;
+
+    [SerializeField]
+    private Text resolutionText;
+
+    private bool fullscreen;
+    private Resolution[] resolutions;
+    private Resolution currentResolution;
+    private int resolutionIndex = 0;
+
     private void Start()
     {
         startButton.Select();
+        fullscreen = Screen.fullScreen;
+        resolutions = Screen.resolutions;
+        currentResolution = Screen.currentResolution;
+
+        // Get current resolution
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if ((resolutions[i].width == currentResolution.width) && (resolutions[i].height == currentResolution.height))
+            {
+                resolutionIndex = i;
+                break;
+            }
+        }
+
+        fullscreenButtonText.text = fullscreen ? "On" : "Off";
+        resolutionText.text = currentResolution.width + " x " + currentResolution.height;
     }
 
     public void ToggleOptions()
     {
         optionsMenu.SetActive(!optionsMenu.activeSelf);
         titleMenu.SetActive(!titleMenu.activeSelf);
+    }
+
+    public void ToggleFullscreen()
+    {
+        fullscreen = !fullscreen;
+        fullscreenButtonText.text = fullscreen ? "On" : "Off";
+    }
+
+    public void IterateResolution()
+    {
+        if ((resolutionIndex + 1) >= resolutions.Length)
+        {
+            resolutionIndex = 0;
+        }
+        else
+        {
+            resolutionIndex++;
+        }
+
+        currentResolution = resolutions[resolutionIndex];
+        resolutionText.text = currentResolution.width + " x " + currentResolution.height;
+    }
+
+    public void Apply()
+    {
+        Screen.SetResolution(currentResolution.width, currentResolution.height, fullscreen);
     }
 }
